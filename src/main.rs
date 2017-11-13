15731a7e1bb9a696f28967d2e3ad359387434513
Afonso Lage <lage.afonso@gmail.com>
2017-11-12 21:37:30 -0300

    Finished basic version of network server engine

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,8 @@ fn main() {
     println!("Starting server...");
     let mut server = network::Server::new();
     server.start("0.0.0.0", 44405);
+
+    for packet in server {
+        println!("Got: {:?}", packet);
+    }
 }
--- src/network/server.rs
+++ src/network/server.rs
@@ -29,9 +29,6 @@ pub enum Error {
 }
 
 pub struct Server {
-    sessions: HashMap<u32, Session>,
-    session_evt_tx: Sender<NetEvent>,
-    session_evt_rx: Receiver<NetEvent>,
     evt_tx: Sender<Event>,
     evt_rx: Receiver<Event>,
 }
@@ -39,26 +36,22 @@ pub struct Server {
 
 impl<'a> Server {
     pub fn new() -> Server {
-        let (stx, srx): (Sender<NetEvent>, Receiver<NetEvent>) = mpsc::channel();
         let (tx, rx): (Sender<Event>, Receiver<Event>) = mpsc::channel();
         Server {
-            sessions: HashMap::new(),
-            session_evt_tx: stx,
-            session_evt_rx: srx,
             evt_tx: tx,
             evt_rx: rx,
         }
     }
 
-    pub fn send_event(&mut self, evt: NetEvent) -> Result<(), Error> {
-        match self.session_evt_tx.send(evt) {
-            Err(why) => {
-                println!("Failed to send server event: {:?}", why);
-                Err(Error::EventTxFailure)
-            }
-            Ok(_) => Ok(()),
-        }
-    }
+    // pub fn send_event(&mut self, evt: NetEvent) -> Result<(), Error> {
+    //     match self.session_evt_tx.send(evt) {
+    //         Err(why) => {
+    //             println!("Failed to send server event: {:?}", why);
+    //             Err(Error::EventTxFailure)
+    //         }
+    //         Ok(_) => Ok(()),
+    //     }
+    // }
 
     pub fn start(&mut self, listen_addr: &'a str, port: u16) {
         let cj_listener = match TcpListener::bind(format!("{}:{}", listen_addr, port)) {
@@ -66,21 +59,21 @@ impl<'a> Server {
             Ok(listener) => listener,
         };
 
-        let cj_tx = self.session_evt_tx.clone();
+        let (stx, srx): (Sender<NetEvent>, Receiver<NetEvent>) = mpsc::channel();
 
         thread::Builder::new()
             .name(format!("TCPServerListener"))
             .spawn(move || {
                 let mut client_index = 1u32;
                 for stream in cj_listener.incoming() {
-                    let session = match Session::new(client_index, stream.unwrap(), cj_tx.clone()) {
+                    let session = match Session::new(client_index, stream.unwrap(), stx.clone()) {
                         Err(_) => continue,
                         Ok(ss) => ss,
                     };
 
                     client_index += 1;
 
-                    match cj_tx.send(NetEvent::Connected(session)) {
+                    match stx.send(NetEvent::Connected(session)) {
                         Err(why) => {
                             println!("Failed to send SessionEvent: {:?}", why);
                             break;
@@ -91,37 +84,34 @@ impl<'a> Server {
             })
             .unwrap();
 
-        println!("Waiting for Session Events.");
-
-        loop {
-            match self.session_evt_rx.recv() {
-                Err(why) => {
-                    println!("Failed to read Sessions RX: {:?}", why);
-                    break;
-                }
-                Ok(evt) => {
-                    println!("Received event {:?}", evt);
-                    self.handle_event(evt);
+        let cj_evt_tx = self.evt_tx.clone();
+        thread::spawn(move || {
+            let mut sessions = HashMap::new();
+            println!("Waiting for Session Events.");
+            loop {
+                match srx.recv() {
+                    Err(why) => {
+                        println!("Failed to read Sessions RX: {:?}", why);
+                        break;
+                    }
+                    Ok(evt) => {
+                        println!("Received event {:?}", evt);
+                        Server::handle_event(&mut sessions, cj_evt_tx.clone(), evt);
+                    }
                 }
             }
-        }
-
-        println!("Server has stopped.");
+        });
     }
 
-    fn parse_packet(&self, buf: &[u8]) -> Option<PacketType> {
+    fn parse_packet(buf: &[u8]) -> Option<PacketType> {
         if buf.len() < 2 {
             println!("Failed to parse packet: Length is too smal: {}", buf.len());
             return None;
         }
 
         match buf[0] {
-            0xC1 => {
-                Some(PacketType::C1(C1Packet::new(&buf[1..])))
-            }
-            0xC2 => {
-                Some(PacketType::C2(C2Packet::new(&buf[1..])))
-            }
+            0xC1 => Some(PacketType::C1(C1Packet::new(&buf[1..]))),
+            0xC2 => Some(PacketType::C2(C2Packet::new(&buf[1..]))),
             _ => {
                 println!("Unsupported code received: {}", buf[0]);
                 None
@@ -129,25 +119,29 @@ impl<'a> Server {
         }
     }
 
-    pub fn handle_event(&mut self, evt: NetEvent) {
+    pub fn handle_event(
+        sessions: &mut HashMap<u32, Session>,
+        evt_tx: Sender<Event>,
+        evt: NetEvent,
+    ) {
         match evt {
             NetEvent::Disconnected(id) => {
-                self.sessions.remove(&id);
-                self.evt_tx.send(Event::ClientDisconnected(id)).ok();
+                sessions.remove(&id);
+                evt_tx.send(Event::ClientDisconnected(id)).ok();
             }
             NetEvent::Connected(session) => {
                 let id = session.id;
-                self.sessions.insert(session.id, session);
-                self.evt_tx.send(Event::ClientConnected(id)).ok();
+                sessions.insert(session.id, session);
+                evt_tx.send(Event::ClientConnected(id)).ok();
             }
-            NetEvent::PacketData(ref buf) => match self.parse_packet(buf) {
+            NetEvent::PacketData(ref buf) => match Server::parse_packet(buf) {
                 None => println!("Invalid packet data received: {:?}", buf),
                 Some(pkt) => {
-                    self.evt_tx.send(Event::ClientPacket(pkt)).ok();
+                    evt_tx.send(Event::ClientPacket(pkt)).ok();
                 }
             },
             NetEvent::PostPacket(ref id, ref buf) => {
-                let mut session = match self.sessions.get_mut(&id) {
+                let mut session = match sessions.get_mut(&id) {
                     None => return,
                     Some(s) => s,
                 };
@@ -163,7 +157,7 @@ impl Iterator for Server {
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.evt_rx.recv() {
-            Err(why) => None,
+            Err(_) => None,
             Ok(evt) => Some(evt),
         }
     }
